f9464a88577112f78c8532ea7bcd44e6a221b5b6
Simo <3dsimopetrelli@gmail.com>
2025-09-27 11:40:21 +0200

    Add diagnostic logs to BW products slider

--- assets/js/bw-products-slide.js
+++ assets/js/bw-products-slide.js
@@ -1,6 +1,8 @@
 (function ($) {
     function initBwProductsSlider($scope) {
+        console.log('>>> initBwProductsSlider called', $scope);
         var $slider = $scope.find('.bw-products-slider');
+        console.log('>>> slider trovato:', $slider.length);
 
         if (!$slider.length) return;
 
@@ -9,15 +11,34 @@
         $slider.addClass('is-initialized');
 
         // Inizializza Flickity con i data-*
+        var autoplay = $slider.data('autoplay') ? parseInt($slider.data('autoplay'), 10) : false;
+        var arrows = $slider.data('arrows') === true || $slider.data('arrows') === 'true';
+        var dots = $slider.data('dots') === true || $slider.data('dots') === 'true';
+        var wrap = $slider.data('wrap') === true || $slider.data('wrap') === 'true';
+        var fade = $slider.data('fade') === true || $slider.data('fade') === 'true';
+        var gap = $slider.data('gap');
+        var columns = $slider.data('columns');
+
+        console.log('>>> settings letti:', {
+            autoplay: autoplay,
+            arrows: arrows,
+            dots: dots,
+            wrap: wrap,
+            fade: fade,
+            gap: gap,
+            columns: columns
+        });
+
         $slider.flickity({
             cellAlign: 'left',
             contain: true,
-            autoPlay: $slider.data('autoplay') ? parseInt($slider.data('autoplay'), 10) : false,
-            prevNextButtons: $slider.data('arrows') === true || $slider.data('arrows') === 'true',
-            pageDots: $slider.data('dots') === true || $slider.data('dots') === 'true',
-            wrapAround: $slider.data('wrap') === true || $slider.data('wrap') === 'true',
-            fade: $slider.data('fade') === true || $slider.data('fade') === 'true'
+            autoPlay: autoplay,
+            prevNextButtons: arrows,
+            pageDots: dots,
+            wrapAround: wrap,
+            fade: fade
         });
+        console.log('>>> Flickity inizializzato su', $slider);
     }
 
     // Hook Elementor: frontend + editor
