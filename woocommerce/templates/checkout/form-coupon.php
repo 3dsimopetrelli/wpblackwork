a6c126976e55756e2e2ea0012cde8f556eb9f244
Simo <3dsimopetrelli@gmail.com>
2025-12-24 00:29:33 +0100

    Scope checkout thumbnail sizing to image

--- woocommerce/templates/checkout/form-coupon.php
+++ woocommerce/templates/checkout/form-coupon.php
@@ -20,4 +20,5 @@ if ( ! wc_coupons_enabled() ) {
 		<input type="text" name="coupon_code" class="input-text" placeholder="<?php esc_attr_e( 'Enter Coupon Code', 'woocommerce' ); ?>" id="coupon_code" value="" />
 		<button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply', 'woocommerce' ); ?>"><?php esc_html_e( 'Apply', 'woocommerce' ); ?></button>
 	</div>
+	<?php wp_nonce_field( 'woocommerce-apply-coupon', 'woocommerce-apply-coupon-nonce' ); ?>
 </form>
