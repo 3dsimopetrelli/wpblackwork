65df3f44265c20ae969adac824a5003238f810e0
Simo <3dsimopetrelli@gmail.com>
2026-01-29 14:16:57 +0100

    CART 2

--- woocommerce/templates/cart/cart.php
+++ woocommerce/templates/cart/cart.php
@@ -66,58 +66,57 @@ do_action('woocommerce_before_cart'); ?>
                                     ?>
                                 </div>
 
-                                <div class="bw-cart-item__details">
-                                    <div class="bw-cart-item__name">
-                                        <?php
-                                        if (!$product_permalink) {
-                                            echo wp_kses_post($product_name . '&nbsp;');
-                                        } else {
-                                            echo wp_kses_post(apply_filters('woocommerce_cart_item_name', sprintf('<a href="%s">%s</a>', esc_url($product_permalink), $_product->get_name()), $cart_item, $cart_item_key));
-                                        }
+                                <div class="bw-cart-item__name" data-title="<?php esc_attr_e('Product', 'woocommerce'); ?>">
+                                    <?php
+                                    if (!$product_permalink) {
+                                        echo wp_kses_post(apply_filters('woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key) . '&nbsp;');
+                                    } else {
+                                        echo wp_kses_post(apply_filters('woocommerce_cart_item_name', sprintf('<a href="%s">%s</a>', esc_url($product_permalink), $_product->get_name()), $cart_item, $cart_item_key));
+                                    }
 
-                                        do_action('woocommerce_after_cart_item_name', $cart_item, $cart_item_key);
+                                    do_action('woocommerce_after_cart_item_name', $cart_item, $cart_item_key);
 
-                                        // Meta data.
-                                        echo wc_get_formatted_cart_item_data($cart_item); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-                                
-                                        // Backorder notification.
-                                        if ($_product->backorders_require_notification() && $_product->is_on_backorder($cart_item['quantity'])) {
-                                            echo wp_kses_post(apply_filters('woocommerce_cart_item_backorder_notification', '<p class="backorder_notification">' . esc_html__('Available on backorder', 'woocommerce') . '</p>', $product_id));
-                                        }
-                                        ?>
-                                    </div>
+                                    // Meta data.
+                                    echo wc_get_formatted_cart_item_data($cart_item); // PHPCS: XSS ok.
+                            
+                                    // Backorder notification.
+                                    if ($_product->backorders_require_notification() && $_product->is_on_backorder($cart_item['quantity'])) {
+                                        echo wp_kses_post(apply_filters('woocommerce_cart_item_backorder_notification', '<p class="backorder_notification">' . esc_html__('Available on backorder', 'woocommerce') . '</p>', $product_id));
+                                    }
+                                    ?>
+                                </div>
 
-                                    <div class="bw-cart-item__price">
-                                        <?php
-                                        echo apply_filters('woocommerce_cart_item_price', WC()->cart->get_product_price($_product), $cart_item, $cart_item_key); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-                                        ?>
-                                    </div>
+                                <div class="bw-cart-item__price" data-title="<?php esc_attr_e('Price', 'woocommerce'); ?>">
+                                    <?php
+                                    echo apply_filters('woocommerce_cart_item_price', WC()->cart->get_product_price($_product), $cart_item, $cart_item_key); // PHPCS: XSS ok.
+                                    ?>
+                                </div>
 
-                                    <div class="bw-cart-item__quantity">
-                                        <?php
-                                        if ($_product->is_sold_individually()) {
-                                            $min_quantity = 1;
-                                            $max_quantity = 1;
-                                        } else {
-                                            $min_quantity = 0;
-                                            $max_quantity = $_product->get_max_purchase_quantity();
-                                        }
+                                <div class="bw-cart-item__quantity"
+                                    data-title="<?php esc_attr_e('Quantity', 'woocommerce'); ?>">
+                                    <?php
+                                    if ($_product->is_sold_individually()) {
+                                        $min_quantity = 1;
+                                        $max_quantity = 1;
+                                    } else {
+                                        $min_quantity = 0;
+                                        $max_quantity = $_product->get_max_purchase_quantity();
+                                    }
 
-                                        $product_quantity = woocommerce_quantity_input(
-                                            array(
-                                                'input_name' => "cart[{$cart_item_key}][qty]",
-                                                'input_value' => $cart_item['quantity'],
-                                                'max_value' => $max_quantity,
-                                                'min_value' => $min_quantity,
-                                                'product_name' => $product_name,
-                                            ),
-                                            $_product,
-                                            false
-                                        );
+                                    $product_quantity = woocommerce_quantity_input(
+                                        array(
+                                            'input_name' => "cart[{$cart_item_key}][qty]",
+                                            'input_value' => $cart_item['quantity'],
+                                            'max_value' => $max_quantity,
+                                            'min_value' => $min_quantity,
+                                            'product_name' => $product_name,
+                                        ),
+                                        $_product,
+                                        false
+                                    );
 
-                                        echo apply_filters('woocommerce_cart_item_quantity', $product_quantity, $cart_item_key, $cart_item); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-                                        ?>
-                                    </div>
+                                    echo apply_filters('woocommerce_cart_item_quantity', $product_quantity, $cart_item_key, $cart_item); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+                                    ?>
                                 </div>
                             </div>
                             <?php
