0b698b814e73cb609b72e6854263465c4a45af43
Simo <3dsimopetrelli@gmail.com>
2026-03-09 17:35:35 +0100

    Update form-coupon.php

--- woocommerce/templates/checkout/form-coupon.php
+++ woocommerce/templates/checkout/form-coupon.php
@@ -4,7 +4,7 @@
  *
  * @see https://woocommerce.com/document/template-structure/
  * @package BW_MEW
- * @version 7.0.1
+ * @version 9.8.0
  */
 
 defined('ABSPATH') || exit;
@@ -14,19 +14,19 @@ if (!wc_coupons_enabled()) {
 }
 
 ?>
-<div class="checkout_coupon woocommerce-form-coupon" data-form-type="coupon">
+<form class="checkout_coupon woocommerce-form-coupon" method="post" id="woocommerce-checkout-form-coupon" data-form-type="coupon">
 	<div class="bw-coupon-fields">
 		<div class="bw-coupon-input-wrapper">
-			<input type="text" name="coupon_code" class="input-text bw-coupon-code-input" placeholder="" value="" />
+			<label for="coupon_code" class="screen-reader-text"><?php esc_html_e('Coupon:', 'woocommerce'); ?></label>
+			<input type="text" name="coupon_code" id="coupon_code" class="input-text bw-coupon-code-input" placeholder="" value="" autocomplete="off" />
 			<label class="bw-floating-label" data-full="<?php esc_attr_e('Enter coupon code', 'woocommerce'); ?>"
 				data-short="<?php esc_attr_e('Coupon code', 'woocommerce'); ?>"><?php esc_html_e('Enter coupon code', 'woocommerce'); ?></label>
 		</div>
-		<button type="button" class="button bw-apply-button" name="apply_coupon"
-			value="<?php esc_attr_e('Apply', 'woocommerce'); ?>"><?php esc_html_e('Apply', 'woocommerce'); ?></button>
+		<button type="submit" class="button bw-apply-button" name="apply_coupon"
+			value="<?php esc_attr_e('Apply coupon', 'woocommerce'); ?>"><?php esc_html_e('Apply', 'woocommerce'); ?></button>
 	</div>
 	<div class="bw-coupon-error" style="display: none;"></div>
-	<input type="hidden" name="woocommerce-apply-coupon-nonce"
-		value="<?php echo esc_attr(wp_create_nonce('woocommerce-apply-coupon')); ?>" />
+	<?php wp_nonce_field('woocommerce-apply-coupon', 'woocommerce-apply-coupon-nonce'); ?>
 	<input type="hidden" name="_wp_http_referer"
 		value="<?php echo esc_attr(wp_unslash($_SERVER['REQUEST_URI'] ?? '')); ?>" />
-</div>
+</form>
