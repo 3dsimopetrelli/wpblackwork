736b934402a2f57f1e22be0a9b393d7ed72153db
Simo <3dsimopetrelli@gmail.com>
2026-03-09 20:11:24 +0100

    Update related.php

--- woocommerce/templates/single-product/related.php
+++ woocommerce/templates/single-product/related.php
@@ -2,10 +2,17 @@
 /**
  * Related Products
  *
- * This template displays related products using the Wall Post card layout.
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/related.php.
  *
- * @package WooCommerce/Templates
- * @version 3.9.0
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see         https://woocommerce.com/document/template-structure/
+ * @package     WooCommerce\Templates
+ * @version     10.3.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -13,6 +20,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( $related_products ) :
+    /**
+     * Ensure all images of related products are lazy loaded by increasing the
+     * current media count to WordPress's lazy loading threshold if needed.
+     * Because wp_increase_content_media_count() is a private function, we
+     * check for its existence before use.
+     */
+    if ( function_exists( 'wp_increase_content_media_count' ) ) {
+        $content_media_count = wp_increase_content_media_count( 0 );
+        if ( $content_media_count < wp_omit_loading_attr_threshold() ) {
+            wp_increase_content_media_count( wp_omit_loading_attr_threshold() - $content_media_count );
+        }
+    }
+
     $heading = apply_filters( 'woocommerce_product_related_products_heading', __( 'Related products', 'woocommerce' ) );
 
     $columns = absint( apply_filters( 'bw_mew_related_products_columns', 3 ) );
@@ -31,7 +51,7 @@ if ( $related_products ) :
         <div class="bw-related-products-grid bw-wallpost-grid">
             <?php foreach ( $related_products as $related_product ) :
                 $post_object = get_post( $related_product->get_id() );
-                setup_postdata( $GLOBALS['post'] =& $post_object );
+                setup_postdata( $GLOBALS['post'] = $post_object ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited, Squiz.PHP.DisallowMultipleAssignments.Found
 
                 $product_id     = $related_product->get_id();
                 $permalink      = function_exists( 'bw_get_safe_product_permalink' )
