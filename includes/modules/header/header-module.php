0498c408a347baedf0b5fc8de2173d01f17c0c98
Simo <3dsimopetrelli@gmail.com>
2026-04-13 15:16:26 +0200

    css

--- includes/modules/header/header-module.php
+++ includes/modules/header/header-module.php
@@ -13,3 +13,13 @@ require_once __DIR__ . '/frontend/assets.php';
 require_once __DIR__ . '/frontend/header-render.php';
 require_once __DIR__ . '/frontend/fragments.php';
 require_once __DIR__ . '/frontend/ajax-search.php';
+
+if (!function_exists('bw_header_register_nav_menus')) {
+    function bw_header_register_nav_menus()
+    {
+        register_nav_menus([
+            'bw_mobile_footer_menu' => __('Footer Menu', 'bw'),
+        ]);
+    }
+}
+add_action('after_setup_theme', 'bw_header_register_nav_menus');
--- includes/modules/header/helpers/menu.php
+++ includes/modules/header/helpers/menu.php
@@ -38,3 +38,39 @@ if (!function_exists('bw_header_render_menu')) {
         return is_string($html) ? $html : '';
     }
 }
+
+if (!function_exists('bw_header_filter_footer_nav_link_class')) {
+    function bw_header_filter_footer_nav_link_class($atts)
+    {
+        if (isset($atts['class']) && !empty($atts['class'])) {
+            $atts['class'] .= ' bw-navigation__mobile-footer-link';
+        } else {
+            $atts['class'] = 'bw-navigation__mobile-footer-link';
+        }
+
+        return $atts;
+    }
+}
+
+if (!function_exists('bw_header_render_footer_menu')) {
+    function bw_header_render_footer_menu($theme_location, $menu_class)
+    {
+        $theme_location = sanitize_key($theme_location);
+        if ($theme_location === '') {
+            return '';
+        }
+
+        add_filter('nav_menu_link_attributes', 'bw_header_filter_footer_nav_link_class', 10, 1);
+        $html = wp_nav_menu([
+            'theme_location' => $theme_location,
+            'menu_class' => $menu_class,
+            'container' => false,
+            'fallback_cb' => '__return_empty_string',
+            'echo' => false,
+            'depth' => 1,
+        ]);
+        remove_filter('nav_menu_link_attributes', 'bw_header_filter_footer_nav_link_class', 10);
+
+        return is_string($html) ? $html : '';
+    }
+}
