093dbf13a598862aecbbaed0463f582e6795fc3c
Simo <3dsimopetrelli@gmail.com>
2026-04-15 22:31:35 +0200

    css

--- includes/widgets/class-bw-static-showcase-widget.php
+++ includes/widgets/class-bw-static-showcase-widget.php
@@ -676,7 +676,9 @@ class Widget_Bw_Static_Showcase extends Widget_Base {
                         <?php if ( $has_cta ) : ?>
                             <div class="bw-slide-showcase-cta">
                                 <a href="<?php echo esc_url( $btn_url ); ?>" class="bw-slide-showcase-arrow" aria-label="<?php echo esc_attr( $button_text ); ?>">
-                                    <span aria-hidden="true">&rsaquo;</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" focusable="false" class="lucide lucide-chevron-right-icon lucide-chevron-right">
+                                        <path d="m9 18 6-6-6-6"/>
+                                    </svg>
                                 </a>
                                 <a href="<?php echo esc_url( $btn_url ); ?>" class="bw-slide-showcase-view-btn">
                                     <?php echo esc_html( $button_text ); ?>
